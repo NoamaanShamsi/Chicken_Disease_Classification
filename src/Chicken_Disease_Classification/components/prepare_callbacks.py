fc4ff84a08ed6c9e5599aeb90a5733dc96c96bf9
NoamaanShamsi <nomshams7186@gmail.com>
2024-06-15 15:40:31 +0530

    Changes done

--- src/Chicken_Disease_Classification/components/prepare_callbacks.py
+++ src/Chicken_Disease_Classification/components/prepare_callbacks.py
@@ -10,16 +10,15 @@ class PrepareCallback:
         self.config = config
 
 
-    
     @property
     def _create_tb_callbacks(self):
         timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
         tb_running_log_dir = os.path.join(
-            self.config.tensorboard_root_log_dir,
+            str(self.config.tensorboard_root_log_dir),
             f"tb_logs_at_{timestamp}",
         )
         return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
-    
+
 
     @property
     def _create_ckpt_callbacks(self):
--- src/Chicken_Disease_Classification/config/configuration.py
+++ src/Chicken_Disease_Classification/config/configuration.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 from Chicken_Disease_Classification.utils.common import read_yaml, create_directories
 from Chicken_Disease_Classification.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig,PrepareCallbacksConfig)
+                                                PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig)
 
 
 
@@ -57,6 +57,7 @@ class ConfigurationManager:
         return prepare_base_model_config
     
 
+
     def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
         config = self.config.prepare_callbacks
         model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
@@ -73,7 +74,27 @@ class ConfigurationManager:
 
         return prepare_callback_config
     
-    
 
 
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
     
